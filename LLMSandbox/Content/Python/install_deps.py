50cc11c413be70b8e90e17693bcb4deaf1fe92b5
Neural VFX <38269894+NeuralVFX@users.noreply.github.com>
2026-08-31 20:33:40 -0700

    Update install_deps.py

--- LLMSandbox/Content/Python/install_deps.py
+++ LLMSandbox/Content/Python/install_deps.py
@@ -3,14 +3,15 @@ import sys
 import os
 import unreal
 
-engine_root = unreal.Paths.convert_relative_path_to_full(unreal.Paths.engine_dir())
-real_python_exe = os.path.normpath(os.path.join(engine_root, "Binaries", "ThirdParty", "Python3", "Win64", "python.exe"))
-script_dir = os.path.dirname(__file__)
-
-project_content = unreal.Paths.project_content_dir()
-target_path = os.path.normpath(os.path.join(project_content, "Python", "Lib", "site-packages"))
-
-subprocess.Popen(
-    [real_python_exe, os.path.join(script_dir, "run_install.py"), real_python_exe, target_path],
-    creationflags=subprocess.CREATE_NEW_CONSOLE
-)
+def install_sandbox_dependencies():
+    engine_root = unreal.Paths.convert_relative_path_to_full(unreal.Paths.engine_dir())
+    real_python_exe = os.path.normpath(os.path.join(engine_root, "Binaries", "ThirdParty", "Python3", "Win64", "python.exe"))
+    script_dir = os.path.dirname(__file__)
+    
+    project_content = unreal.Paths.project_content_dir()
+    target_path = os.path.normpath(os.path.join(project_content, "Python", "Lib", "site-packages"))
+    
+    subprocess.Popen(
+        [real_python_exe, os.path.join(script_dir, "run_install.py"), real_python_exe, target_path],
+        creationflags=subprocess.CREATE_NEW_CONSOLE
+    )
